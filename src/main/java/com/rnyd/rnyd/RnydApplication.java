6a65b53c7f8bb83318f022cb3bc2b95b29754b04
rmoralesinformatica <rmoralesinformatica@gmail.com>
2025-05-03 16:55:47 +0200

    Primer commit

---
+++ src/main/java/com/rnyd/rnyd/RnydApplication.java
@@ -0,0 +1,15 @@
+package com.rnyd.rnyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnydApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(RnydApplication.class, args);
+
+	}
+
+}
