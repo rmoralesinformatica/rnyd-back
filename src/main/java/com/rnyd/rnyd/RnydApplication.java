1f0606e64e8fe095b28475e0d430c626e0c99b35
rmoralesinformatica <rmoralesinformatica@gmail.com>
2025-05-03 17:07:04 +0200

    prueba de commit 2

--- src/main/java/com/rnyd/rnyd/RnydApplication.java
+++ src/main/java/com/rnyd/rnyd/RnydApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RnydApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		SpringApplication.run(RnydApplication.class, args);
 
 	}
