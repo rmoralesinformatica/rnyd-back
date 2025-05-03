b819ac5121a0b8bd4863b01dad2378f4e762309e
rmoralesinformatica <rmoralesinformatica@gmail.com>
2025-05-03 16:55:47 +0200

    Primer commit

---
+++ src/test/java/com/rnyd/rnyd/RnydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnyd.rnyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
