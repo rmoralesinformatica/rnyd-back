23c69103e81cb96bfb46a8997193e132c8f93a5e
rmoralesinformatica <rmoralesinformatica@gmail.com>
2025-05-04 22:42:35 +0200

    revision

--- src/main/java/com/rnyd/rnyd/RnydApplication.java
+++ src/main/java/com/rnyd/rnyd/RnydApplication.java
@@ -2,10 +2,14 @@ package com.rnyd.rnyd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
+@EntityScan(basePackages = "com.rnyd.rnyd.model")
+@SpringBootApplication(scanBasePackages = "com.rnyd.rnyd")
+
 public class RnydApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RnydApplication.class, args);
 
