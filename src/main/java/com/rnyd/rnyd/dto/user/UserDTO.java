9d0b624d772040805e927b952ff44f321266f83b
rmoralesinformatica <rmoralesinformatica@gmail.com>
2025-05-04 18:52:37 +0200

    Creacion de nuevas columnas de UserDTO

---
+++ src/main/java/com/rnyd/rnyd/dto/user/UserDTO.java
@@ -0,0 +1,71 @@
+package com.rnyd.rnyd.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+public class UserDTO {
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("surname")
+    private String surname;
+
+    @JsonProperty("birth_date")
+    private LocalDate birth_date;
+
+    @JsonProperty("keyword")
+    private String keyword;
+
+    public UserDTO(String name, String email, String surname, LocalDate birth_date, String keyword) {
+        this.name = name;
+        this.email = email;
+        this.surname = surname;
+        this.birth_date = birth_date;
+        this.keyword = keyword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(LocalDate birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
+
